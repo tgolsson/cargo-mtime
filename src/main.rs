3adaa6e85eb0dd723651e217c8709573d2aa1233
Tom Solberg <ts@MSI>
2024-02-17 12:16:35 +0100

    works with rusqlite

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
-use std::sync::Arc;
+//! Small helper binary using Sqlite to maintain an mtime cache
+//! to avoid unnecessary rebuilds when using Cargo from a sandbox.
+//! This is a workaround for the lack of a `mtime` cache in Cargo.
+//!
+//! Given the environment path `CARGO_MTIME_ROOT_DIR` and
+//! `CARGO_MTIME_DB_PATH` this utility will set the mtime of every
+//! file in the root directory to the mtime stored in the database,
+//! iff the sha256 of the file matches the sha256 stored in the
+//! database.
 
+use async_walkdir::DirEntry;
+use async_walkdir::Filtering;
 use async_walkdir::WalkDir;
+use filetime::FileTime;
 use futures::StreamExt;
-/// Small helper binary using Sqlite to maintain an mtime cache
-/// to avoid unnecessary rebuilds when using Cargo from a sandbox.
-/// This is a workaround for the lack of a `mtime` cache in Cargo.
-///
-/// Given the environment path `CARGO_MTIME_ROOT_DIR` and
-/// `CARGO_MTIME_DB_PATH` this utility will set the mtime of every
-/// file in the root directory to the mtime stored in the database,
-/// iff the sha256 of the file matches the sha256 stored in the
-/// database.
-use sqlite::{Connection, ConnectionThreadSafe, State, Value};
-use tracing::Instrument;
+use rusqlite::Connection;
+use rusqlite::ToSql;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::sync::Arc;
+use tokio::sync::mpsc::unbounded_channel;
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::sync::mpsc::UnboundedSender;
+use tokio::sync::oneshot;
+use tokio::sync::OwnedSemaphorePermit;
+use tokio::sync::Semaphore;
 
 #[derive(Debug, Clone)]
 struct Config {
     root_dir: String,
     db_path: String,
 }
+
+#[derive(Default, Debug)]
+struct Metrics {
+    files_processed: AtomicUsize,
+    files_was_directory: AtomicUsize,
+    files_mtime_restored: AtomicUsize,
+    files_mtime_skipped: AtomicUsize,
+    files_mtime_updated: AtomicUsize,
+
+    database_mtime_queries: AtomicUsize,
+    database_mtime_hits: AtomicUsize,
+    database_mtime_misses: AtomicUsize,
+
+    database_mtime_write: AtomicUsize,
+}
+
+type DatabaseQuery = (String, String, oneshot::Sender<Option<i64>>);
 
 impl Config {
     fn from_env() -> Self {
@@ -36,18 +64,15 @@ impl Config {
         }
         #[cfg(not(debug_assertions))]
         Self {
-            root_dir: std::env::var("CARGO_MTIME_ROOT_sha256: String: String").unwrap(),
+            root_dir: std::env::var("CARGO_MTIME_ROOT").unwrap(),
             db_path: std::env::var("CARGO_MTIME_DB_PATH").unwrap(),
         }
     }
 }
 
-fn init_database(config: &Config) -> Result<ConnectionThreadSafe, sqlite::Error> {
-    let span = tracing::info_span!("init_database", root_dir = %config.root_dir, db_path = %config.db_path);
-    let _guard = span.enter();
-
+fn init_database(config: &Config) -> Result<Connection, rusqlite::Error> {
     // Create the database if it doesn't exist
-    let conn = sqlite::Connection::open_thread_safe(&config.db_path)?;
+    let conn = rusqlite::Connection::open(&config.db_path)?;
 
     tracing::info!("Opened database at {}", &config.db_path);
     {
@@ -57,19 +82,23 @@ fn init_database(config: &Config) -> Result<ConnectionThreadSafe, sqlite::Error>
             key TEXT PRIMARY KEY,
 			value TEXT
 		)",
+            (),
         )?;
 
         let mut version = None;
 
         let mut stmt = conn.prepare("SELECT value FROM metadata WHERE key = 'version'")?;
-        while let State::Row = stmt.next()? {
-            version = Some(stmt.read::<String, _>(0)?);
+        for v in stmt.query_map([], |row| row.get::<_, String>(0))? {
+            version = Some(v?);
         }
 
         match version {
             None => {
                 tracing::info!("Found no version in metadata table, initializing database");
-                conn.execute("INSERT INTO metadata (key, value) VALUES ('version', '1')")?;
+                conn.execute(
+                    "INSERT INTO metadata (key, value) VALUES ('version', '1')",
+                    (),
+                )?;
 
                 // Create mtime table if it doesn't exist
                 conn.execute(
@@ -78,11 +107,13 @@ fn init_database(config: &Config) -> Result<ConnectionThreadSafe, sqlite::Error>
 					sha256 TEXT,
 					mtime INTEGER
 				)",
+                    (),
                 )?;
 
                 // Create index on path + sha256
                 conn.execute(
                     "CREATE INDEX IF NOT EXISTS mtime_path_sha256 ON mtime (path, sha256)",
+                    (),
                 )?;
             }
             Some(version) => {
@@ -96,136 +127,155 @@ fn init_database(config: &Config) -> Result<ConnectionThreadSafe, sqlite::Error>
     Ok(conn)
 }
 
-fn try_get_mtime(conn: &ConnectionThreadSafe, path: &str, sha256: String) -> Option<i64> {
+fn try_get_mtime(conn: &Connection, path: &str, sha256: &str, metrics: &Metrics) -> Option<i64> {
     tracing::trace!(
-							shasum = %sha256,
-							path = %path,
-							"try_get_mtime");
+        shasum = %sha256,
+        path = %path,
+        "try_get_mtime"
+    );
+
+    metrics
+        .database_mtime_queries
+        .fetch_add(1, Ordering::Relaxed);
+
     let mut stmt = conn
-        .prepare("SELECT mtime FROM mtime WHERE path = ? AND sha256 = ?")
-        .unwrap();
-    stmt.bind::<&[(_, Value)]>(&[(1, path.into()), (2, sha256.into())][..])
+        .prepare_cached("SELECT mtime FROM mtime WHERE path = ? AND sha256 = ?")
         .unwrap();
 
-    if let State::Row = stmt.next().unwrap() {
-        Some(stmt.read::<i64, _>(0).unwrap())
+    let res = stmt
+        .query(&[path, sha256])
+        .unwrap()
+        .next()
+        .unwrap()
+        .map(|row| row.get(0));
+
+    if res.is_some() {
+        metrics.database_mtime_hits.fetch_add(1, Ordering::Relaxed);
     } else {
-        None
+        metrics
+            .database_mtime_misses
+            .fetch_add(1, Ordering::Relaxed);
     }
-}
-fn update_database(
-    connection: &ConnectionThreadSafe,
-    batch: &[(String, String, i64)],
-) -> Result<(), sqlite::Error> {
-    let mut stmt = connection
-        .prepare("INSERT OR REPLACE INTO mtime (path, sha256, mtime) VALUES (?, ?, ?)")?;
 
-    dbg!(batch);
-    for (path, sha256, mtime) in batch {
-        stmt.bind::<&[(_, Value)]>(
-            &[
-                (1, path.as_str().into()),
-                (2, sha256.as_str().into()),
-                (3, (*mtime).into()),
-            ][..],
-        )?;
-        stmt.next()?;
-        stmt.reset()?;
-    }
+    res.and_then(std::result::Result::ok)
+}
+
+fn update_database(
+    connection: &Connection,
+    batch: &[(String, String, i64)],
+    metrics: &Metrics,
+) -> Result<(), rusqlite::Error> {
+    let stmt = r#"
+INSERT OR REPLACE INTO mtime (path, sha256, mtime) VALUES "#
+        .to_string()
+        + &batch
+            .iter()
+            .map(|_| "(?, ?, ?)")
+            .collect::<Vec<_>>()
+            .join(", ")
+        + r#";
+"#;
+    let mut batch_stmt = connection.prepare(&stmt)?;
+
+    batch_stmt.execute(rusqlite::params_from_iter(batch.iter().flat_map(
+        |(path, sha256, mtime)| {
+            vec![
+                path as &dyn ToSql,
+                sha256 as &dyn ToSql,
+                mtime as &dyn ToSql,
+            ]
+        },
+    )))?;
+
+    metrics
+        .database_mtime_write
+        .fetch_add(batch.len(), Ordering::Relaxed);
+
     Ok(())
 }
 
-fn main() {
+async fn database_lookup_task(
+    mut query_rx: UnboundedReceiver<DatabaseQuery>,
+    metrics: Arc<Metrics>,
+    read_connection: Connection,
+) {
+    while let Some((path, sha256, response)) = query_rx.recv().await {
+        let path: String = path;
+        let sha256: String = sha256;
+
+        let mtime = try_get_mtime(&read_connection, &path, &sha256, metrics.as_ref());
+        tracing::trace!(path = %path, sha256 = %sha256, mtime = ?mtime, "query_handle");
+
+        response.send(mtime).unwrap();
+    }
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    tracing::info!("Installing panic hook");
     let default_panic = std::panic::take_hook();
     std::panic::set_hook(Box::new(move |info| {
         default_panic(info);
         std::process::exit(1);
     }));
-    tracing_subscriber::fmt::init();
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .unhandled_panic(tokio::runtime::UnhandledPanic::ShutdownRuntime)
-        .enable_io()
-        .worker_threads(10)
-        .build()
-        .unwrap().block_on(async {
 
+    let metrics = Arc::new(Metrics::default());
 
     let config = Config::from_env();
-    let connection = Arc::new(init_database(&config).unwrap());
+    let connection = init_database(&config).unwrap();
 
-    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
-    let root_dir = config.root_dir.clone();
+    // We'll use a single worker to write to the database, taking requests from all the other workers.
+    let (tx, rx) = unbounded_channel();
+    let handle = tokio::spawn(gather_submit_db(rx, connection, metrics.clone()));
 
-    let conn = connection.clone();
-    // We'll generate one worker that'll deal with updating the database,
-    // and a bunch of workers that traverse the filesystem.
-    let handle = tokio::spawn(async move {
+    // We'll also use a single worker to read from the database, taking requests from all the other workers.
+    let read_connection = init_database(&config).unwrap();
+    let (query_tx, query_rx) = unbounded_channel::<DatabaseQuery>();
+    let query_handle = tokio::spawn(database_lookup_task(
+        query_rx,
+        metrics.clone(),
+        read_connection,
+    ));
 
-        let mut current_batch: Vec<(String, String, i64)> = vec![];
-
-        while let Some((path, sha256, mtime)) = rx.recv().await {
-            current_batch.push((path, sha256, mtime));
-
-            if current_batch.len() >= 100 {
-                update_database(&connection, &current_batch).unwrap();
-                current_batch.clear();
-            }
+    // We'll use a semaphore to limit the number of concurrent file operations, to avoid running out of file descriptors.
+    let semaphore = Arc::new(Semaphore::new(512));
+    let mut entries = WalkDir::new(&config.root_dir).filter(|entry| async move {
+        if let Some(true) = entry
+            .path()
+            .file_name()
+            .map(|f| f.to_string_lossy().starts_with('.'))
+        {
+            return Filtering::IgnoreDir;
         }
 
-        if !current_batch.is_empty() {
-            tracing::info!(count = current_batch.len(), "Flushing remaining batch");
-            update_database(&connection, &current_batch).unwrap();
+        // any directory containing .rustc_info.json is a rustc build directory
+        if entry.path().join(".rustc_info.json").exists() {
+            return Filtering::IgnoreDir;
         }
+
+        Filtering::Continue
     });
 
-    let mut entries = WalkDir::new(&root_dir);
     loop {
         match entries.next().await {
             Some(Ok(entry)) => {
+                metrics.files_processed.fetch_add(1, Ordering::Relaxed);
+
                 let is_file = entry.file_type().await.map_or(false, |t| t.is_file());
+
                 if !is_file {
+                    metrics.files_was_directory.fetch_add(1, Ordering::Relaxed);
                     continue;
                 }
 
-                let conn = conn.clone();
                 let tx = tx.clone();
-                tracing::trace!(path = %entry.path().to_string_lossy(), "spawn_worker");
-                tokio::spawn(async move {
-                    let span = tracing::span!(tracing::Level::INFO, "check", path = %entry.path().to_string_lossy());
+                let query_tx = query_tx.clone();
 
-                    let path = entry.path().to_string_lossy().to_string();
-                    let sha256 = sha256::try_async_digest(path.clone())
-                        .instrument(span.clone())
-                        .await
-                        .unwrap();
-
-                    if let Some(mtime) = try_get_mtime(&conn, &path, sha256.clone()) {
-                        span.in_scope(|| {
-                            tracing::trace!(mtime = %mtime, "set_file_mtime");
-                            filetime::set_file_mtime(
-                                &path,
-                                filetime::FileTime::from_unix_time(mtime, 0),
-                            )
-                            .unwrap();
-                        });
-                    } else {
-                        span.in_scope(|| tracing::trace!("send_to_worker"));
-                        let mtime = filetime::FileTime::from_system_time(
-                            entry
-                                .metadata()
-                                .instrument(span.clone())
-                                .await
-                                .unwrap()
-                                .modified()
-                                .unwrap(),
-                        )
-                        .unix_seconds();
-                        tx.send((path, sha256, mtime))
-                            .instrument(span)
-                            .await
-                            .unwrap();
-                    }
-                });
+                let semaphore = semaphore.clone();
+                let permit = semaphore.acquire_owned().await.unwrap();
+                tokio::spawn(manage_mtime(entry, query_tx, permit, tx, metrics.clone()));
             }
             Some(Err(e)) => {
                 eprintln!("error: {}", e);
@@ -236,7 +286,65 @@ fn main() {
     }
 
     drop(tx);
+    drop(query_tx);
+    drop(semaphore);
+
     handle.await.unwrap();
-			panic!("fooo");
-	});
+    query_handle.await.unwrap();
+}
+
+async fn gather_submit_db(
+    mut rx: UnboundedReceiver<(String, String, i64)>,
+    connection: Connection,
+    metrics: Arc<Metrics>,
+) {
+    let mut current_batch: Vec<(String, String, i64)> = vec![];
+
+    while let Some((path, sha256, mtime)) = rx.recv().await {
+        current_batch.push((path, sha256, mtime));
+
+        if current_batch.len() >= 100 {
+            update_database(&connection, &current_batch, metrics.as_ref()).unwrap();
+            current_batch.clear();
+        }
+    }
+
+    if !current_batch.is_empty() {
+        update_database(&connection, &current_batch, metrics.as_ref()).unwrap();
+    }
+}
+
+async fn manage_mtime(
+    entry: DirEntry,
+    query_tx: UnboundedSender<DatabaseQuery>,
+    permit: OwnedSemaphorePermit,
+    tx: UnboundedSender<(String, String, i64)>,
+    metrics: Arc<Metrics>,
+) {
+    let path = entry.path();
+    let (sha256, metadata) = tokio::join!(sha256::try_async_digest(&path), entry.metadata(),);
+    let sha256 = sha256.unwrap();
+
+    let mtime_on_disk =
+        FileTime::from_system_time(metadata.unwrap().modified().unwrap()).unix_seconds();
+
+    let string_path = path.to_string_lossy().to_string();
+    let (response_tx, response_rx) = oneshot::channel();
+    query_tx
+        .send((string_path.clone(), sha256.clone(), response_tx))
+        .unwrap();
+
+    if let Some(mtime) = response_rx.await.unwrap() {
+        if mtime != mtime_on_disk {
+            metrics.files_mtime_restored.fetch_add(1, Ordering::Relaxed);
+            filetime::set_file_mtime(&path, FileTime::from_unix_time(mtime, 0)).unwrap();
+            drop(permit);
+        } else {
+            metrics.files_mtime_skipped.fetch_add(1, Ordering::Relaxed);
+        }
+    } else {
+        drop(permit);
+        metrics.files_mtime_updated.fetch_add(1, Ordering::Relaxed);
+        tx.send((string_path, sha256, mtime_on_disk)).unwrap()
+    }
 }
